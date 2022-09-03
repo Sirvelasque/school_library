d1e4bda51d70f1ae8ad15c39a7f92cf2fd8ca747
Sirvelasque <velasquezwalter10@yahoo.es>
2022-09-03 16:04:37 -0600

    Create capitalize decorator

---
+++ capitalize_decorator.rb
@@ -0,0 +1,12 @@
+requiere './decorator.rb'
+
+class CapitalizeDecorator < Decorator
+  def initialize(nameable)
+    super
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name.capitalize
+  end
+end
